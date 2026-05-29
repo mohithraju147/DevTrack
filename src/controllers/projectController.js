34b2bcf91350fc431e9e50f5a524ee66f76bb911
Mohith Raju <mohithraju@Mohiths-MacBook-Air.local>
2026-05-29 18:49:43 +0530

    feat: connect projects api to database

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -1,11 +1,20 @@
-const getProjects = (req, res) => {
-  res.status(200).json([
-    {
-      id: 1,
-      name: "DevTrack",
-      status: "active"
-    }
-  ]);
+const pool = require("../config/database");
+
+const getProjects = async (req, res) => {
+  try {
+    const result = await pool.query(
+      "SELECT * FROM projects"
+    );
+
+    res.json(result.rows);
+
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Database Error"
+    });
+  }
 };
 
 module.exports = {
